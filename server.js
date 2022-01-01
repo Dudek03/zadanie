a37bea8dc08bf7fcde2f37b5933c7c66308132a1
Dawid <dawid_03@onet.eu>
2022-01-01 22:13:25 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
+
+let dane = {imie: "Dawid",
+            nazwisko: "Młodzianowski",
+            klasa: "3I2",
+            grupa: "2"}
+
+app.get("/", function (req, res) {
+    res.send("<h1>my app on heroku!</h1>")
+})
+
+app.get("/data", function (req, res) {
+    res.send(dane)
+})
+
+app.listen(PORT, function () {
+    console.log("start serwera na porcie " + PORT)
+})
